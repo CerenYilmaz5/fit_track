495ef963724276c6822eb796326cccdc33d3336b
BasarKocabas <kbasarkocabas@gmail.com>
2024-11-21 22:11:55 +0300

    Workout Plan and Deit Plan UI
    I simply created workout plan and diet plan UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+
+void changePage(Widget newPage) {
+  navigatorKey.currentState?.push(
+    MaterialPageRoute(builder: (context) => newPage),
+  );
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,115 +20,219 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const Row(
+            children: [
+              MyStatsTextBox(buttonName: "Vücut Kitle Endeksi:"),
+              MyStatsTextBox(buttonName: "Vücut Yağ Oranı:"),
+              MyStatsTextBox(buttonName: "Günlük Kalori Miktarı:"),
+            ],
+          ),
+
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: 300,
+            height: 100,
+            child: ElevatedButton.icon(
+              onPressed: () => changePage(const PlaningPage(subject: "Workout",)),
+              label: const Text("Workout Plan", style: TextStyle(fontSize: 30),),
+              icon: const Icon(Icons.fitness_center),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: 300,
+            height: 100,
+            child: ElevatedButton.icon(
+              onPressed: () => changePage(const PlaningPage(subject: "Diet",)),
+              label: const Text("Daily Diet", style: TextStyle(fontSize: 30),),
+              icon: const Icon(Icons.fastfood),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class MyStatsTextBox extends StatelessWidget{
+
+  const MyStatsTextBox({
+    super.key,
+    required this.buttonName,
+  });
+
+  final String buttonName;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> words = buttonName.split(" ").map((word){
+      return Text(
+        word,
+        style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic, color: Theme.of(context).colorScheme.surface),
+      );
+    }).toList();
+    // TODO: implement build
+    return Container(
+      height: 300,
+      width: 130,
+      padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
+      margin: const EdgeInsets.fromLTRB(4, 30, 2, 100),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(15),
+
+      ),
+      child: Column(
+        children: words,
+      ),
+    );
+  }
+}
+
+
+class PlaningPage extends StatelessWidget{
+
+  final String subject;
+
+  const PlaningPage({
+    required this.subject,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(5, 20, 5, 0),
+                    child: ElevatedButton.icon(
+                      onPressed: () => changePage(const MyHomePage()),
+                      label: const Text("Back"),
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                  ),
+
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    height:50,
+                    width: 280,
+                    child:  Card(
+                      child: Text(
+                        "$subject Plan",
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              DayButton(day: 1, subject: subject ),
+              DayButton(day: 2, subject: subject ),
+              DayButton(day: 3, subject: subject ),
+              DayButton(day: 4, subject: subject ),
+              DayButton(day: 5, subject: subject ),
+              DayButton(day: 6, subject: subject ),
+              DayButton(day: 7, subject: subject ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class DayButton extends StatelessWidget{
+
+  final String subject;
+
+  const DayButton({
+    super.key,
+    required this.day,
+    required this.subject
+  });
+
+  final int day;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      height: 75,
+      margin: const EdgeInsets.fromLTRB(35,20,20,0),
+      child: ElevatedButton(
+          onPressed: () => changePage(DayPage(day: day, subject:  subject,)),
+          child: Text(
+            "Day $day",
+            style: const TextStyle(fontSize: 20,),
+          )
+      ),
+    );
+  }
+}
+
+class DayPage extends StatelessWidget{
+
+  final String subject;
+
+  const DayPage({
+    super.key,
+    required this.day,
+    required this.subject
+  });
+
+  final int day;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () => changePage( PlaningPage(subject: subject,)),
+                    label: const Text("Back"),
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                  const SizedBox(width: 20,),
+                  Text("DAY $day $subject planning")
+                ],
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
+
