2e1f6b8db90a155334bdcdffd5f37573696f01ff
BasarKocabas <kbasarkocabas@gmail.com>
2024-11-25 21:44:01 +0300

    CopyPaste1
    I copied my side of the code from the branch called "Fit_track_BASARINKISIM"

---
+++ lib/StatsPlanningScreen.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+
+void changePage(Widget newPage) {
+  navigatorKey.currentState?.push(
+    MaterialPageRoute(builder: (context) => newPage),
+  );
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const Row(
+            children: [
+              MyStatsTextBox(buttonName: "Vücut Kitle Endeksi:"),
+              MyStatsTextBox(buttonName: "Vücut Yağ Oranı:"),
+              MyStatsTextBox(buttonName: "Günlük Kalori Miktarı:"),
+            ],
+          ),
+
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: 300,
+            height: 100,
+            child: ElevatedButton.icon(
+              onPressed: () => changePage(const PlaningPage(subject: "Workout",)),
+              label: const Text("Workout Plan", style: TextStyle(fontSize: 30),),
+              icon: const Icon(Icons.fitness_center),
+            ),
+          ),
+
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: 300,
+            height: 100,
+            child: ElevatedButton.icon(
+              onPressed: () => changePage(const PlaningPage(subject: "Diet",)),
+              label: const Text("Daily Diet", style: TextStyle(fontSize: 30),),
+              icon: const Icon(Icons.fastfood),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyStatsTextBox extends StatelessWidget{
+
+  const MyStatsTextBox({
+    super.key,
+    required this.buttonName,
+  });
+
+  final String buttonName;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> words = buttonName.split(" ").map((word){
+      return Text(
+        word,
+        style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic, color: Theme.of(context).colorScheme.surface),
+      );
+    }).toList();
+    // TODO: implement build
+    return Container(
+      height: 300,
+      width: 130,
+      padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
+      margin: const EdgeInsets.fromLTRB(4, 30, 2, 100),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(15),
+
+      ),
+      child: Column(
+        children: words,
+      ),
+    );
+  }
+}
+
+
+class PlaningPage extends StatelessWidget{
+
+  final String subject;
+
+  const PlaningPage({
+    required this.subject,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(5, 20, 5, 0),
+                    child: ElevatedButton.icon(
+                      onPressed: () => changePage(const MyHomePage()),
+                      label: const Text("Back"),
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                  ),
+
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    height:50,
+                    width: 280,
+                    child:  Card(
+                      child: Text(
+                        "$subject Plan",
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              DayButton(day: 1, subject: subject ),
+              DayButton(day: 2, subject: subject ),
+              DayButton(day: 3, subject: subject ),
+              DayButton(day: 4, subject: subject ),
+              DayButton(day: 5, subject: subject ),
+              DayButton(day: 6, subject: subject ),
+              DayButton(day: 7, subject: subject ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class DayButton extends StatelessWidget{
+
+  final String subject;
+
+  const DayButton({
+    super.key,
+    required this.day,
+    required this.subject
+  });
+
+  final int day;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      height: 75,
+      margin: const EdgeInsets.fromLTRB(35,20,20,0),
+      child: ElevatedButton(
+          onPressed: () => changePage(DayPage(day: day, subject:  subject,)),
+          child: Text(
+            "Day $day",
+            style: const TextStyle(fontSize: 20,),
+          )
+      ),
+    );
+  }
+}
+
+class DayPage extends StatelessWidget{
+
+  final String subject;
+
+  const DayPage({
+    super.key,
+    required this.day,
+    required this.subject
+  });
+
+  final int day;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () => changePage( PlaningPage(subject: subject,)),
+                    label: const Text("Back"),
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                  const SizedBox(width: 20,),
+                  Text("DAY $day $subject planning")
+                ],
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
+
