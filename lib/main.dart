90069cd070ca903d6c68303f63b16f3d49329f44
Ceren Yılmaz <crnylmz652@gmail.com>
2024-11-25 21:30:27 +0300

    Login screen
    I created login screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,222 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,//Sağ üstteki Debug yazısını kaldırıyor
+      title: 'Fit Life',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true, //Uygulamanın güncel kalması için
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FitnessLoginPage(title: 'Fit Track '),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class FitnessLoginPage extends StatefulWidget {
+  const FitnessLoginPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FitnessLoginPage> createState() => _FitnessLoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _FitnessLoginPageState extends State<FitnessLoginPage> {
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.white,
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0) ,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Icon(Icons.fitness_center,
+                    size: 100,
+                    color: Colors.green),
+                const SizedBox(height: 20),
+                const Text(
+                  'Welcome to Fit Track!',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height:10),
+                const Text(
+                  "Stay healhty,stay strong.Let's achieve your goals together!",
+                  textAlign: TextAlign.center,
+                  style:TextStyle(
+                    fontSize:16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height:30),
+                TextField(
+                    onChanged: (value){
+                      setState(() {
+                        email=value;
+                      });
+                    },
+                    decoration : InputDecoration(
+                        labelText: 'E-Mail',
+                        border:OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        )
+                    )
+
+                ),
+                const SizedBox(height:30),
+                TextField(
+                    onChanged: (value){
+                      setState(() {
+                        password=value;
+                      });
+                    },
+                    decoration : InputDecoration(
+                        labelText: 'Password',
+                        border:OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        )
+                    )
+                ),
+                const SizedBox(height: 30,),
+                SizedBox(
+                  width: double.infinity,
+                  child:ElevatedButton(
+                    onPressed: (){
+                      if(email.isNotEmpty && password.isNotEmpty){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Welcome: $email')),
+                        );
+                      }else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text ('Please fill all fields!'))
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      padding: const EdgeInsets.symmetric(vertical: 15),// Log in butonunu kalınlaştırıyo
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: const Text(
+                      'Log In',style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ForgotPasswordPage()),
+                        );
+                      },
+                      child :const Text (
+                        'Forgot Password',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: (){
+
+                      },
+                      child :const Text (
+                        'Sign Up',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
+
+//Forgot passaword sayfası
+class ForgotPasswordPage extends StatelessWidget{
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    String email = '';
+
+    return  Scaffold(
+        appBar: AppBar(
+          title: const Text('Forgot Passaword'),
+          backgroundColor: Colors.green,
+        ),
+        body:Padding(
+          padding:  const EdgeInsets.all(20),
+          child : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children : [
+              const Text(
+                'Enter an email,we send you a link for reset your password.',
+                style :TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                onChanged: (value){
+                  email = value;
+                },
+                decoration : InputDecoration(
+                  labelText: 'E-Mail',
+                  border:OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: (){
+                  if(email.isNotEmpty){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text (
+                            'Link to reset your passaword send $email'),
+                      ),
+                    );
+                  }else{
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Please enter an email')),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child : const Text ('Send'),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
